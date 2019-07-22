a61470b5ab5bd26b0d21ae8832db2452a5509402
Ken Cruz <ken@kencruz.ca>
2019-07-22 09:38:24 -0400

    first render of image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::error::Error;
+use std::fs::{create_dir_all, File};
+use std::io::prelude::*;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let nx: u32 = 200;
+    let ny: u32 = 100;
+    let mut output = format!("P3\n{} {}\n255\n", nx, ny);
+    for j in (0..ny).rev() {
+        for i in 0..nx {
+            let r: f32 = i as f32 / nx as f32;
+            let g: f32 = j as f32 / ny as f32;
+            let b: f32 = 0.2;
+            let ir = (255.99 * r) as u32;
+            let ig = (255.99 * g) as u32;
+            let ib = (255.99 * b) as u32;
+            output = format!("{}{} {} {}\n", output, ir, ig, ib);
+        }
+    }
+
+    let dir = Path::new("out/");
+    let path = Path::new("out/image.ppm");
+    let display = path.display();
+
+    match create_dir_all(&dir) {
+        Err(why) => panic!("couldn't create dir {:?}: {}", &dir, why.description()),
+        Ok(_) => println!("successfully created directory {:?}", &dir),
+    }
+
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}", display, why.description()),
+        Ok(file) => file,
+    };
+
+    match file.write_all(output.as_bytes()) {
+        Err(why) => panic!("couldn't write to {}: {}", display, why.description()),
+        Ok(_) => println!("successfully wrote to {}", display),
+    }
 }
