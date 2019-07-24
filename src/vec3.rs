e0aa0402eb0051d9297053b06a4ad0f37caa8d82
Ken Cruz <ken@kencruz.ca>
2019-07-24 12:41:54 -0400

    added vec3 basic operator overloading

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,15 +1,18 @@
+use std::ops::Add;
+use std::ops::Sub;
+use std::ops::Mul;
+use std::ops::Div;
+
+
 pub struct Vec3 {
     x: f32,
     y: f32,
     z: f32,
-    r: f32,
-    g: f32,
-    b: f32,
 }
 
 impl Vec3 {
-    pub fn new(r: f32, g: f32, b: f32) -> Vec3 {
-        Vec3 { x: 0.0, y: 0.0, z: 0.0, r, g, b }
+    pub fn new(x: f32, y: f32, z: f32) -> Vec3 {
+        Vec3 { x: 0.0, y: 0.0, z: 0.0,}
     }
 
     pub fn x(&self) -> f32 {
@@ -25,14 +28,110 @@ impl Vec3 {
     }
 
     pub fn r(&self) -> f32 {
-        self.r
+        self.x
     }
 
     pub fn g(&self) -> f32 {
-        self.g
+        self.y
     }
 
     pub fn b(&self) -> f32 {
-        self.b
+        self.z
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
+
+impl Mul for Vec3 {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self {
+        Self {
+            x: self.x * other.x,
+            y: self.y * other.y,
+            z: self.z * other.z,
+        }
+    }
+}
+
+impl Mul<f32> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, rhs: f32) -> Self {
+        Self {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
+    }
+}
+
+impl Mul<Vec3> for f32 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Vec3 {
+        Vec3 {
+            x: self * rhs.x,
+            y: self * rhs.y,
+            z: self * rhs.z,
+        }
+    }
+}
+
+impl Div for Vec3 {
+    type Output = Self;
+
+    fn div(self, other: Self) -> Self {
+        Self {
+            x: self.x / other.x,
+            y: self.y / other.y,
+            z: self.z / other.z,
+        }
+    }
+}
+
+impl Div<f32> for Vec3 {
+    type Output = Self;
+
+    fn div(self, rhs: f32) -> Self {
+        Self {
+            x: self.x / rhs,
+            y: self.y / rhs,
+            z: self.z / rhs,
+        }
+    }
+}
+
+impl Div<Vec3> for f32 {
+    type Output = Vec3;
+
+    fn div(self, rhs: Vec3) -> Vec3 {
+        Vec3 {
+            x: self / rhs.x,
+            y: self / rhs.y,
+            z: self / rhs.z,
+        }
     }
 }
