e4abb741b8f1af75a2b994875b999b7498331d46
Ken Cruz <ken@kencruz.ca>
2019-07-25 11:01:05 -0400

    implemented full vec3 operations

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,9 +1,6 @@
-use std::ops::Add;
-use std::ops::Sub;
-use std::ops::Mul;
-use std::ops::Div;
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
 
-
+#[derive(Debug)]
 pub struct Vec3 {
     x: f32,
     y: f32,
@@ -12,7 +9,7 @@ pub struct Vec3 {
 
 impl Vec3 {
     pub fn new(x: f32, y: f32, z: f32) -> Vec3 {
-        Vec3 { x: 0.0, y: 0.0, z: 0.0,}
+        Vec3 { x, y, z }
     }
 
     pub fn x(&self) -> f32 {
@@ -22,7 +19,7 @@ impl Vec3 {
     pub fn y(&self) -> f32 {
         self.y
     }
-    
+
     pub fn z(&self) -> f32 {
         self.z
     }
@@ -38,6 +35,31 @@ impl Vec3 {
     pub fn b(&self) -> f32 {
         self.z
     }
+
+    pub fn length(&self) -> f32 {
+        self.squared_length().sqrt()
+    }
+
+    pub fn squared_length(&self) -> f32 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn unit(&self) -> Vec3 {
+        let k: f32 = 1.0 / self.length();
+        Vec3::new(self.x * k, self.y * k, self.z * k)
+    }
+
+    pub fn dot(&self, other: &Vec3) -> f32 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+
+    pub fn cross(&self, other: &Vec3) -> Vec3 {
+        Vec3::new(
+            self.y * other.z - self.z * other.y,
+            -(self.x * other.z - self.z * other.x),
+            self.x * other.y - self.y * other.x,
+        )
+    }
 }
 
 impl Add for Vec3 {
@@ -52,6 +74,16 @@ impl Add for Vec3 {
     }
 }
 
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        };
+    }
+}
+
 impl Sub for Vec3 {
     type Output = Self;
 
@@ -61,6 +93,16 @@ impl Sub for Vec3 {
             y: self.y - other.y,
             z: self.z - other.z,
         }
+    }
+}
+
+impl SubAssign for Vec3 {
+    fn sub_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        };
     }
 }
 
@@ -100,6 +142,26 @@ impl Mul<Vec3> for f32 {
     }
 }
 
+impl MulAssign for Vec3 {
+    fn mul_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x * other.x,
+            y: self.y * other.y,
+            z: self.z * other.z,
+        };
+    }
+}
+
+impl MulAssign<f32> for Vec3 {
+    fn mul_assign(&mut self, other: f32) {
+        *self = Self {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other,
+        };
+    }
+}
+
 impl Div for Vec3 {
     type Output = Self;
 
@@ -132,6 +194,38 @@ impl Div<Vec3> for f32 {
             x: self / rhs.x,
             y: self / rhs.y,
             z: self / rhs.z,
+        }
+    }
+}
+
+impl DivAssign for Vec3 {
+    fn div_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x / other.x,
+            y: self.y / other.y,
+            z: self.z / other.z,
+        };
+    }
+}
+
+impl DivAssign<f32> for Vec3 {
+    fn div_assign(&mut self, other: f32) {
+        *self = Self {
+            x: self.x / other,
+            y: self.y / other,
+            z: self.z / other,
+        };
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Self;
+
+    fn neg(self) -> Self {
+        Self {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
         }
     }
 }
