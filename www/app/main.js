23f4118709624e1de85a41bfba63fa433d4bfc9c
Ken Cruz <ken@kencruz.ca>
2019-11-14 02:15:44 -0500

    multiple workers for parallel computing

--- www/app/main.js
+++ www/app/main.js
@@ -1,30 +1,87 @@
-const worker = new Worker("./worker.js");
-
 const submitButton = document.querySelector("#submitButton");
 const result = document.querySelector("#result");
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-worker.addEventListener("message", ev => {
-  const message = ev.data;
-  if (message.allGood === true) {
+const WIDTH = 200;
+const HEIGHT = 100;
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+
+const IMG = new Uint8ClampedArray(WIDTH * HEIGHT * 4);
+let count = HEIGHT;
+let received = 0;
+let t0;
+
+submitButton.addEventListener("click", async () => {
+  let workers = [];
+  for (let i = 0; i < 4; i++) {
+    workers[i] = new Worker("./worker.js");
+  }
+
+  await Promise.all(workers.map(x => loaded(x)));
+
+  for (let i = 0; i < workers.length; i++) {
+    let worker = workers[i];
+
+    worker.addEventListener("message", ev => {
+      const message = ev.data;
+      if (message.allGood === true) {
         ctx.putImageData(message.imgData, 0, 0);
         result.textContent = message.time;
-  } else if (message.allGood === "processing") {
-        result.textContent = message.status;
-  } else if (message.allGood === false) {
+      } else if (message.allGood === "ready") {
+        console.log(`worker ${i} is ready`);
+        count--;
+        worker.postMessage({
+          job: true,
+          count: count
+        });
+      } else if (message.allGood === false) {
         result.textContent = "Something went wrong! " + message.error;
-  } else {
-        let imageData = new ImageData(message, 200);
-        ctx.putImageData(imageData, 0, 0);
+      } else {
+        received++;
+        for (let i = 0; i < message.imgRow.length; i++) {
+          IMG[((HEIGHT - message.row) * WIDTH * 4) + i] = message.imgRow[i];
+        }
+        if (received % 10 === 0) {
+          let imageData = new ImageData(IMG, WIDTH, HEIGHT);
+          ctx.putImageData(imageData, 0, 0);
+        }
+        count--;
+        if (count >= 0) {
+          worker.postMessage({
+            job: true,
+            count: count
+          });
+        }
+        result.textContent = `${received * 100 / HEIGHT}% complete`
+        if (received === HEIGHT) {
+          let imageData = new ImageData(IMG, WIDTH, HEIGHT);
+          ctx.putImageData(imageData, 0, 0);
+          let t1 = performance.now();
+          result.textContent = `done in ${t1 - t0} ms`;
+        }
+      }
+    });
   }
+
+  t0 = performance.now();
+
+  import("../../pkg")
+    .then(wasm => {
+      const world = wasm.scene_gen_json();
+      for (let i = 0; i < workers.length; i++) {
+        workers[i].postMessage({
+          init: true,
+          width: WIDTH,
+          height: HEIGHT,
+          world: world
+        });
+      }
+    });
 });
 
-submitButton.addEventListener("click", () => {
-    import("../../pkg").then(wasm => {
-        const WIDTH = 200;
-        const HEIGHT = 100;
-        const world = wasm.scene_gen_json();
-        worker.postMessage({init: true, width: WIDTH, height: HEIGHT, world: world});
-    });
-} );
+// helper to ensure worker is loaded before taking any jobs
+const loaded = w => new Promise(r => w.addEventListener("message", r, {
+  once: true
+}));
--- www/worker/worker.js
+++ www/worker/worker.js
@@ -1,29 +1,27 @@
-import("../../pkg").then(wasm => {
+import("../../pkg")
+  .then(wasm => {
     let WIDTH;
     let HEIGHT;
     let world;
     let scene;
 
     self.addEventListener("message", ev => {
-        const msg = ev.data;
-        if (msg.init) {
-            WIDTH = msg.width;
-            HEIGHT = msg.height;
-            world = msg.world;
-            scene = wasm.Scene.new(WIDTH, HEIGHT, world);
-            const arr = new Uint8ClampedArray(WIDTH * HEIGHT * 4);
-
-            for (let y = HEIGHT - 1; y >= 0; y--) {
-                let row = scene.image_row(y);
-                for (let i = 0; i < row.length; i++) {
-                    arr[((HEIGHT - y - 1) * WIDTH * 4) + i] = row[i]
-                }
-                self.postMessage({ allGood: "processing", status: `${(HEIGHT - y) * 100 / HEIGHT}% complete` });
-                self.postMessage(arr);
-            }
-
-        }
+      const msg = ev.data;
+      if (msg.init) {
+        WIDTH = msg.width;
+        HEIGHT = msg.height;
+        world = msg.world;
+        scene = wasm.Scene.new(WIDTH, HEIGHT, world);
+        self.postMessage({
+          allGood: "ready"
+        });
+      } else if (msg.job) {
+        let row = scene.image_row(msg.count);
+        self.postMessage({
+          row: msg.count,
+          imgRow: row
+        });
+      }
     });
+      self.postMessage("loaded");
   });
-
-
